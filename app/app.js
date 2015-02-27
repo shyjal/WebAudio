2e61ddc4245ebf36499dbe55e46eed94fe977479
Shyjal Raazi <shyjal99@gmail.com>
2015-02-27 16:41:15 +0530

    volume

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ sound.play();
 
 window.onload = function(){
    window.s1=new Sound('assets/animals001.mp3');
-   window.s2=new Sound('assets/animals002.mp3');
+   window.s2=new Sound('assets/animals003.mp3');
 }
 
 
@@ -18,6 +18,7 @@ window.startPlay=function(){
 
 	//Object.play(volume,delayInSecs)
 
-	s1.play(100,0);
-	s1.play(50,1);
+	s1.play(50,0);
+	s2.play(100,1);
+	s1.play(10,4);
 }
